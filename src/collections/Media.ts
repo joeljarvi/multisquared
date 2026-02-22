9cd56f6c7d09450a0a360ec50a157dfcda60a710
joeljarvi <joeljarvi@live.se>
2026-02-22 10:39:12 +0100

    ändring i media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,21 +2,26 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
+
   access: {
     read: () => true,
   },
+
+  upload: true,
+
   fields: [
     {
       name: 'alt',
       type: 'text',
       required: true,
     },
-
     {
       name: 'externalUrl',
       type: 'text',
-      label: 'Supabase Image URL',
+      label: 'Large Image URL (Supabase)',
+      admin: {
+        description: 'Use this for images over 5MB or uploaded directly to Supabase',
+      },
     },
   ],
-  upload: true,
 }

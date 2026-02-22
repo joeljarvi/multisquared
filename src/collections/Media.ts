b35e1321789ae2e5cf3612ae6d6136d93c300f91
joeljarvi <joeljarvi@live.se>
2026-02-22 11:34:25 +0100

    fix med med media ochj works

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -2,29 +2,15 @@ import type { CollectionConfig } from 'payload'
 
 export const Media: CollectionConfig = {
   slug: 'media',
-
   access: {
     read: () => true,
   },
-
-  upload: {
-    staticDir: 'media',
-    mimeTypes: ['image/*', 'video/*'],
-  },
-
   fields: [
     {
       name: 'alt',
       type: 'text',
       required: true,
     },
-    {
-      name: 'externalUrl',
-      type: 'text',
-      label: 'Large Image URL (Supabase)',
-      admin: {
-        description: 'Use this for images over 5MB or uploaded directly to Supabase',
-      },
-    },
   ],
+  upload: true,
 }
